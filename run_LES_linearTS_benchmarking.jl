f3586ded559fcc2749c75a4fae6067db07699b38
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2023-10-27 14:11:41 -0400

    pure benchmarking script

---
+++ run_LES_linearTS_benchmarking.jl
@@ -0,0 +1,235 @@
+using Oceananigans
+using Oceananigans.Units
+using JLD2
+using FileIO
+using Printf
+using CairoMakie
+using Oceananigans.Grids: halo_size
+using Oceananigans.Operators
+using Oceananigans.AbstractOperations: KernelFunctionOperation
+using Oceananigans.BuoyancyModels
+using SeawaterPolynomials.TEOS10
+using SeawaterPolynomials
+using Random
+using Statistics
+using ArgParse
+
+import Dates
+
+function parse_commandline()
+    s = ArgParseSettings()
+  
+    @add_arg_table! s begin
+      "--QU"
+        help = "surface momentum flux (m²/s²)"
+        arg_type = Float64
+        default = 0.
+      "--QT"
+        help = "surface temperature flux (°C m/s)"
+        arg_type = Float64
+        default = 0.
+      "--QS"
+        help = "surface salinity flux (m/s g/kg)"
+        arg_type = Float64
+        default = 0.
+      "--T_surface"
+        help = "surface temperature (°C)"
+        arg_type = Float64
+        default = 20.
+      "--S_surface"
+        help = "surface salinity (g/kg)"
+        arg_type = Float64
+        default = 35.
+      "--dTdz"
+        help = "Initial temperature gradient (°C/m)"
+        arg_type = Float64
+        default = 1 / 256
+      "--dSdz"
+        help = "Initial salinity gradient (g/kg/m))"
+        arg_type = Float64
+        default = -0.25 / 256
+      "--f"
+        help = "Coriolis parameter (s⁻¹)"
+        arg_type = Float64
+        default = 1e-4
+      "--Nz"
+        help = "Number of grid points in z-direction"
+        arg_type = Int64
+        default = 128
+      "--Nx"
+        help = "Number of grid points in x-direction"
+        arg_type = Int64
+        default = 256
+      "--Ny"
+        help = "Number of grid points in y-direction"
+        arg_type = Int64
+        default = 256
+      "--Lz"
+        help = "Domain depth"
+        arg_type = Float64
+        default = 256.
+      "--Lx"
+        help = "Domain width in x-direction"
+        arg_type = Float64
+        default = 512.
+      "--Ly"
+        help = "Domain width in y-direction"
+        arg_type = Float64
+        default = 512.
+      "--dt"
+        help = "Initial timestep to take (seconds)"
+        arg_type = Float64
+        default = 0.1
+      "--max_dt"
+        help = "Maximum timestep (seconds)"
+        arg_type = Float64
+        default = 10. * 60
+      "--stop_time"
+        help = "Stop time of simulation (days)"
+        arg_type = Float64
+        default = 4.
+      "--time_interval"
+        help = "Time interval of output writer (minutes)"
+        arg_type = Float64
+        default = 10.
+      "--fps"
+        help = "Frames per second of animation"
+        arg_type = Float64
+        default = 15.
+      "--pickup"
+        help = "Whether to pickup from latest checkpoint"
+        arg_type = Bool
+        default = true
+      "--advection"
+        help = "Advection scheme used"
+        arg_type = String
+        default = "WENO9nu1e-5"
+    end
+    return parse_args(s)
+end
+
+args = parse_commandline()
+
+Random.seed!(123)
+
+const Lz = args["Lz"]
+const Lx = args["Lx"]
+const Ly = args["Ly"]
+
+const Nz = args["Nz"]
+const Nx = args["Nx"]
+const Ny = args["Ny"]
+
+const Qᵁ = args["QU"]
+const Qᵀ = args["QT"]
+const Qˢ = args["QS"]
+
+const Pr = 1
+
+if args["advection"] == "WENO9nu1e-5"
+    advection = WENO(order=9)
+    closure = ScalarDiffusivity(ν=1e-5, κ=1e-5/Pr)
+elseif args["advection"] == "WENO9nu0"
+    advection = WENO(order=9)
+    closure = nothing
+elseif args["advection"] == "WENO9AMD"
+    advection = WENO(order=9)
+    closure = AnisotropicMinimumDissipation()
+elseif args["advection"] == "AMD"
+    advection = CenteredSecondOrder()
+    closure = AnisotropicMinimumDissipation()
+end
+
+const f = args["f"]
+
+const dTdz = args["dTdz"]
+const dSdz = args["dSdz"]
+
+const T_surface = args["T_surface"]
+const S_surface = args["S_surface"]
+
+const pickup = args["pickup"]
+
+size_halo = 5
+
+grid = RectilinearGrid(GPU(), Float64,
+                       size = (Nx, Ny, Nz),
+                       halo = (size_halo, size_halo, size_halo),
+                       x = (0, Lx),
+                       y = (0, Ly),
+                       z = (-Lz, 0),
+                       topology = (Periodic, Periodic, Bounded))
+
+noise(x, y, z) = rand() * exp(z / 8)
+
+T_initial(x, y, z) = dTdz * z + T_surface
+S_initial(x, y, z) = dSdz * z + S_surface
+
+T_initial_noisy(x, y, z) = T_initial(x, y, z) + 1e-6 * noise(x, y, z)
+S_initial_noisy(x, y, z) = S_initial(x, y, z) + 1e-6 * noise(x, y, z)
+
+T_bcs = FieldBoundaryConditions(top=FluxBoundaryCondition(Qᵀ), bottom=GradientBoundaryCondition(dTdz))
+S_bcs = FieldBoundaryConditions(top=FluxBoundaryCondition(Qˢ), bottom=GradientBoundaryCondition(dSdz))
+u_bcs = FieldBoundaryConditions(top=FluxBoundaryCondition(Qᵁ))
+
+const eos = TEOS10EquationOfState()
+
+damping_rate = 1/5minute
+
+T_target(x, y, z, t) = T_initial(x, y, z)
+S_target(x, y, z, t) = S_initial(x, y, z)
+
+bottom_mask = GaussianMask{:z}(center=-grid.Lz, width=grid.Lz/10)
+
+uvw_sponge = Relaxation(rate=damping_rate, mask=bottom_mask)
+T_sponge = Relaxation(rate=damping_rate, mask=bottom_mask, target=T_target)
+S_sponge = Relaxation(rate=damping_rate, mask=bottom_mask, target=S_target)
+
+model = NonhydrostaticModel(; 
+            grid = grid,
+            closure = closure,
+            coriolis = FPlane(f=f),
+            buoyancy = SeawaterBuoyancy(equation_of_state=eos),
+            tracers = (:T, :S, :b),
+            timestepper = :RungeKutta3,
+            advection = advection,
+            forcing = (u=uvw_sponge, v=uvw_sponge, w=uvw_sponge, T=T_sponge, S=S_sponge),
+            boundary_conditions = (T=T_bcs, S=S_bcs, u=u_bcs)
+            )
+
+set!(model, T=T_initial_noisy, S=S_initial_noisy)
+
+T = model.tracers.T
+S = model.tracers.S
+u, v, w = model.velocities
+
+simulation = Simulation(model, Δt=args["dt"]second, stop_time=args["stop_time"]days)
+# simulation = Simulation(model, Δt=args["dt"]second, stop_time=100minutes)
+
+wizard = TimeStepWizard(max_change=1.05, max_Δt=args["max_dt"]minutes, cfl=0.6)
+simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
+
+wall_clock = [time_ns()]
+
+function print_progress(sim)
+    @printf("%s [%05.2f%%] i: %d, t: %s, wall time: %s, max(u): (%6.3e, %6.3e, %6.3e) m/s, max(T) %6.3e, max(S) %6.3e, next Δt: %s\n",
+            Dates.now(),
+            100 * (sim.model.clock.time / sim.stop_time),
+            sim.model.clock.iteration,
+            prettytime(sim.model.clock.time),
+            prettytime(1e-9 * (time_ns() - wall_clock[1])),
+            maximum(abs, sim.model.velocities.u),
+            maximum(abs, sim.model.velocities.v),
+            maximum(abs, sim.model.velocities.w),
+            maximum(abs, sim.model.tracers.T),
+            maximum(abs, sim.model.tracers.S),
+            prettytime(sim.Δt))
+
+    wall_clock[1] = time_ns()
+
+    return nothing
+end
+
+simulation.callbacks[:print_progress] = Callback(print_progress, IterationInterval(100))
+
+run!(simulation)

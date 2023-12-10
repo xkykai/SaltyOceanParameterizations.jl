fafe14af7da7696e7403ad3f2723825ccae754d4
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2023-12-10 15:24:41 -0500

    update spectra calculation to reduce memory

--- calculate_spectra.jl
+++ calculate_spectra.jl
@@ -5,28 +5,39 @@ using LinearAlgebra
 using SparseArrays
 using JLD2
 
-FILE_DIRs = ["./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_AMD_Lxz_64.0_128.0_Nxz_32_64_f"]
+FILE_DIRs = [
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_AMD_Lxz_64.0_128.0_Nxz_32_64_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_WENO9nu0_Lxz_64.0_128.0_Nxz_32_64_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_WENO9nu1e-5_Lxz_64.0_128.0_Nxz_32_64_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_WENO9AMD_Lxz_64.0_128.0_Nxz_32_64_f",
 
-b_datas = [FieldTimeSeries("$(FILE_DIR)/instantaneous_fields_b.jld2", "b") for FILE_DIR in FILE_DIRs]
-u_datas = [FieldTimeSeries("$(FILE_DIR)/instantaneous_fields_u.jld2", "u") for FILE_DIR in FILE_DIRs]
-v_datas = [FieldTimeSeries("$(FILE_DIR)/instantaneous_fields_v.jld2", "v") for FILE_DIR in FILE_DIRs]
-w_datas = [FieldTimeSeries("$(FILE_DIR)/instantaneous_fields_w.jld2", "w") for FILE_DIR in FILE_DIRs]
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.0005_QB_1.0e-7_b_0.0_AMD_Lxz_64.0_128.0_Nxz_32_64_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.0005_QB_1.0e-7_b_0.0_WENO9nu0_Lxz_64.0_128.0_Nxz_32_64_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.0005_QB_1.0e-7_b_0.0_WENO9nu1e-5_Lxz_64.0_128.0_Nxz_32_64_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.0005_QB_1.0e-7_b_0.0_WENO9AMD_Lxz_64.0_128.0_Nxz_32_64_f",
 
-face_to_centers = [spzeros(size(interior(u_datas[i][1]), 3), size(interior(w_datas[i][1]), 3)) for i in eachindex(FILE_DIRs)]
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_AMD_Lxz_64.0_128.0_Nxz_64_128_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_WENO9nu0_Lxz_64.0_128.0_Nxz_64_128_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_WENO9nu1e-5_Lxz_64.0_128.0_Nxz_64_128_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_WENO9AMD_Lxz_64.0_128.0_Nxz_64_128_f",
 
-for face_to_center in face_to_centers
-    for i in axes(face_to_center, 1)
-        face_to_center[i, i:i+1] .= 0.5
-    end
-end
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.0005_QB_1.0e-7_b_0.0_AMD_Lxz_64.0_128.0_Nxz_64_128_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.0005_QB_1.0e-7_b_0.0_WENO9nu0_Lxz_64.0_128.0_Nxz_64_128_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.0005_QB_1.0e-7_b_0.0_WENO9nu1e-5_Lxz_64.0_128.0_Nxz_64_128_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.0005_QB_1.0e-7_b_0.0_WENO9AMD_Lxz_64.0_128.0_Nxz_64_128_f",
 
-w_center_datas = [zeros(size(data)) for data in u_datas]
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_AMD_Lxz_64.0_128.0_Nxz_128_256_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_WENO9nu0_Lxz_64.0_128.0_Nxz_128_256_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_WENO9nu1e-5_Lxz_64.0_128.0_Nxz_128_256_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_WENO9AMD_Lxz_64.0_128.0_Nxz_128_256_f",
 
-for (num, w_center) in enumerate(w_center_datas)
-    for i in axes(w_center, 1), j in axes(w_center, 2), l in axes(w_center, 4)
-        w_center[i, j, :, l] .= face_to_centers[num] * interior(w_datas[num][l], i, j, :)
-    end
-end
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.0005_QB_1.0e-7_b_0.0_AMD_Lxz_64.0_128.0_Nxz_128_256_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.0005_QB_1.0e-7_b_0.0_WENO9nu0_Lxz_64.0_128.0_Nxz_128_256_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.0005_QB_1.0e-7_b_0.0_WENO9nu1e-5_Lxz_64.0_128.0_Nxz_128_256_f",
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.0005_QB_1.0e-7_b_0.0_WENO9AMD_Lxz_64.0_128.0_Nxz_128_256_f",
+
+    "./LES/linearb_turbulencestatistics_dbdz_1.953125e-5_QU_-0.001_QB_1.0e-7_b_0.0_AMD_Lxz_64.0_128.0_Nxz_256_512_f"
+]
 
 function calculate_spectra(data::FieldTimeSeries)
     Nx, Ny, Nz = data.grid.Nx, data.grid.Ny, data.grid.Nz
@@ -61,11 +72,33 @@ function calculate_spectra(data, Nx, Ny, Nz, Δx, Δy)
     return (; spectra²_kx, spectra²_ky, kx, ky)
 end
 
-b_spectras = [calculate_spectra(data) for data in b_datas]
-u_spectras = [calculate_spectra(data) for data in u_datas]
-v_spectras = [calculate_spectra(data) for data in v_datas]
-w_center_spectras = [calculate_spectra(data, b_datas[i].grid.Nx, b_datas[i].grid.Ny, b_datas[i].grid.Nz, b_datas[i].grid.Δxᶜᵃᵃ, b_datas[i].grid.Δyᵃᶜᵃ) for (i, data) in enumerate(w_center_datas)]
+function calculate_spectra(FILE_DIRs::Vector{String})
+    for FILE_DIR in FILE_DIRs
+        @info "Calculating spectra for $(FILE_DIR)"
+        b_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_fields_b.jld2", "b")
+        u_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_fields_u.jld2", "u")
+        v_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_fields_v.jld2", "v")
+        w_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_fields_w.jld2", "w")
 
-for i in eachindex(FILE_DIRs)
-    jldsave("$(FILE_DIRs[i])/spectra.jld2"; b=b_spectras[i], u=u_spectras[i], v=v_spectras[i], w=w_center_spectras[i])
+        face_to_center = spzeros(size(interior(u_data[1]), 3), size(interior(w_data[1]), 3))
+
+        for i in axes(face_to_center, 1)
+            face_to_center[i, i:i+1] .= 0.5
+        end
+
+        w_center_data = zeros(size(u_data))
+
+        for i in axes(w_center_data, 1), j in axes(w_center_data, 2), l in axes(w_center_data, 4)
+            w_center_data[i, j, :, l] .= face_to_center * interior(w_data[l], i, j, :)
+        end
+
+        b_spectras = calculate_spectra(b_data)
+        u_spectras = calculate_spectra(u_data)
+        v_spectras = calculate_spectra(v_data)
+        w_center_spectras = calculate_spectra(w_center_data, b_data.grid.Nx, b_data.grid.Ny, b_data.grid.Nz, b_data.grid.Δxᶜᵃᵃ, b_data.grid.Δyᵃᶜᵃ)
+
+        jldsave("$(FILE_DIR)/spectra.jld2"; b=b_spectras, u=u_spectras, v=v_spectras, w=w_center_spectras)
+    end
 end
+
+calculate_spectra(FILE_DIRs)

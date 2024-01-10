0fbb94abc28254bf3e8516c1346dac4644cbbec7
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-01-10 13:40:54 -0500

    make animation for experiments

--- run_LES_linearb_experiments.jl
+++ run_LES_linearb_experiments.jl
@@ -302,31 +302,6 @@ uw_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "uw")
 vw_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "vw")
 wb_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "wb")
 
-u_udot∇u_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "u_udot∇u")
-v_udot∇v_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "v_udot∇v")
-b_udot∇b_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "b_udot∇b")
-w_udot∇w_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "w_udot∇w")
-
-u_udot∇u′_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "u_udot∇u′")
-v_udot∇v′_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "v_udot∇v′")
-b_udot∇b′_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "b_udot∇b′")
-w_udot∇w′_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "w_udot∇w′")
-
-u_∇dotνₑ∇u_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "u_∇dotνₑ∇u")
-v_∇dotνₑ∇v_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "v_∇dotνₑ∇v")
-b_∇dotκₑ∇b_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "b_∇dotκₑ∇b")
-w_∇dotνₑ∇w_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "w_∇dotνₑ∇w")
-
-u_∇dotνₑ∇u′_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "u_∇dotνₑ∇u′")
-v_∇dotνₑ∇v′_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "v_∇dotνₑ∇v′")
-b_∇dotκₑ∇b′_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "b_∇dotκₑ∇b′")
-w_∇dotνₑ∇w′_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "w_∇dotνₑ∇w′")
-
-∂u²∂t_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "∂u²∂t")
-∂v²∂t_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "∂v²∂t")
-∂b²∂t_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "∂b²∂t")
-∂w²∂t_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "∂w²∂t")
-
 Nt = length(bbar_data.times)
 
 xC = bbar_data.grid.xᶜᵃᵃ[1:Nx]
@@ -335,7 +310,7 @@ zC = bbar_data.grid.zᵃᵃᶜ[1:Nz]
 
 zF = uw_data.grid.zᵃᵃᶠ[1:Nz+1]
 ##
-fig = Figure(resolution=(2100, 2100))
+fig = Figure(size=(2100, 2100))
 
 axubar = Axis(fig[1, 1], title="<u>", xlabel="m s⁻¹", ylabel="z")
 axvbar = Axis(fig[1, 2], title="<v>", xlabel="m s⁻¹", ylabel="z")
@@ -345,19 +320,6 @@ axuw = Axis(fig[2, 1], title="uw", xlabel="m² s⁻²", ylabel="z")
 axvw = Axis(fig[2, 2], title="vw", xlabel="m² s⁻²", ylabel="z")
 axwb = Axis(fig[2, 3], title="wb", xlabel="m² s⁻³", ylabel="z")
 
-axuadvection = Axis(fig[3, 1], title="u advection", ylabel="z")
-axvadvection = Axis(fig[3, 3], title="v advection", ylabel="z")
-axbadvection = Axis(fig[1, 4], title="b advection", ylabel="z")
-axwadvection = Axis(fig[4, 1], title="w advection", ylabel="z")
-
-axudissipation = Axis(fig[3, 2], title="u dissipation", ylabel="z")
-axvdissipation = Axis(fig[3, 4], title="v dissipation", ylabel="z")
-axbdissipation = Axis(fig[2, 4], title="b dissipation", ylabel="z")
-axwdissipation = Axis(fig[4, 2], title="w dissipation", ylabel="z")
-
-ax∂TKE∂t = Axis(fig[4, 3], title="0.5 ∂<u² + v² + w²>/∂t", ylabel="z")
-ax∂b²∂t = Axis(fig[4, 4], title="∂<b²>/∂t", ylabel="z")
-
 function find_min(a...)
     return minimum(minimum.([a...]))
 end
@@ -375,21 +337,6 @@ uwlim = (minimum(uw_data[1, 1, :, startframe_lim:end]), maximum(uw_data[1, 1, :,
 vwlim = (minimum(vw_data[1, 1, :, startframe_lim:end]), maximum(vw_data[1, 1, :, startframe_lim:end]))
 wblim = (minimum(wb_data[1, 1, :, startframe_lim:end]), maximum(wb_data[1, 1, :, startframe_lim:end]))
 
-u_udot∇ulim = (find_min(u_udot∇u_data, u_udot∇u′_data), find_max(u_udot∇u_data, u_udot∇u′_data))
-v_udot∇vlim = (find_min(v_udot∇v_data, v_udot∇v′_data), find_max(v_udot∇v_data, v_udot∇v′_data))
-b_udot∇blim = (find_min(b_udot∇b_data, b_udot∇b′_data), find_max(b_udot∇b_data, b_udot∇b′_data))
-w_udot∇wlim = (find_min(w_udot∇w_data, w_udot∇w′_data), find_max(w_udot∇w_data, w_udot∇w′_data))
-
-u_∇dotνₑ∇ulim = (find_min(u_∇dotνₑ∇u_data, u_∇dotνₑ∇u′_data), find_max(u_∇dotνₑ∇u_data, u_∇dotνₑ∇u′_data))
-v_∇dotνₑ∇vlim = (find_min(v_∇dotνₑ∇v_data, v_∇dotνₑ∇v′_data), find_max(v_∇dotνₑ∇v_data, v_∇dotνₑ∇v′_data))
-b_∇dotκₑ∇blim = (find_min(b_∇dotκₑ∇b_data, b_∇dotκₑ∇b′_data), find_max(b_∇dotκₑ∇b_data, b_∇dotκₑ∇b′_data))
-w_∇dotνₑ∇wlim = (find_min(w_∇dotνₑ∇w_data, w_∇dotνₑ∇w′_data), find_max(w_∇dotνₑ∇w_data, w_∇dotνₑ∇w′_data))
-
-∂TKE∂tlim = (minimum(0.5 .* (∂u²∂t_data .+ ∂v²∂t_data .+ ∂w²∂t_data)), 
-             maximum(0.5 .* (∂u²∂t_data .+ ∂v²∂t_data .+ ∂w²∂t_data)))
-
-∂b²∂tlim = (minimum(∂b²∂t_data), maximum(∂b²∂t_data))
-
 n = Observable(1)
 
 time_str = @lift "Qᵁ = $(Qᵁ), Qᴮ = $(Qᴮ), Time = $(round(bbar_data.times[$n]/24/60^2, digits=3)) days"
@@ -403,29 +350,6 @@ uwₙ = @lift interior(uw_data[$n], 1, 1, :)
 vwₙ = @lift interior(vw_data[$n], 1, 1, :)
 wbₙ = @lift interior(wb_data[$n], 1, 1, :)
 
-u_udot∇uₙ = @lift interior(u_udot∇u_data[$n], 1, 1, :)
-v_udot∇vₙ = @lift interior(v_udot∇v_data[$n], 1, 1, :)
-b_udot∇bₙ = @lift interior(b_udot∇b_data[$n], 1, 1, :)
-w_udot∇wₙ = @lift interior(w_udot∇w_data[$n], 1, 1, :)
-
-u_udot∇u′ₙ = @lift interior(u_udot∇u′_data[$n], 1, 1, :)
-v_udot∇v′ₙ = @lift interior(v_udot∇v′_data[$n], 1, 1, :)
-b_udot∇b′ₙ = @lift interior(b_udot∇b′_data[$n], 1, 1, :)
-w_udot∇w′ₙ = @lift interior(w_udot∇w′_data[$n], 1, 1, :)
-
-u_∇dotνₑ∇uₙ = @lift interior(u_∇dotνₑ∇u_data[$n], 1, 1, :)
-v_∇dotνₑ∇vₙ = @lift interior(v_∇dotνₑ∇v_data[$n], 1, 1, :)
-b_∇dotκₑ∇bₙ = @lift interior(b_∇dotκₑ∇b_data[$n], 1, 1, :)
-w_∇dotνₑ∇wₙ = @lift interior(w_∇dotνₑ∇w_data[$n], 1, 1, :)
-
-u_∇dotνₑ∇u′ₙ = @lift interior(u_∇dotνₑ∇u′_data[$n], 1, 1, :)
-v_∇dotνₑ∇v′ₙ = @lift interior(v_∇dotνₑ∇v′_data[$n], 1, 1, :)
-b_∇dotκₑ∇b′ₙ = @lift interior(b_∇dotκₑ∇b′_data[$n], 1, 1, :)
-w_∇dotνₑ∇w′ₙ = @lift interior(w_∇dotνₑ∇w′_data[$n], 1, 1, :)
-
-∂TKE∂tₙ = @lift 0.5 .* (interior(∂u²∂t_data[$n], 1, 1, :) .+ interior(∂v²∂t_data[$n], 1, 1, :) .+ interior(∂w²∂t_data[$n], 1, 1, :))
-∂b²∂tₙ = @lift interior(∂b²∂t_data[$n], 1, 1, :)
-
 lines!(axubar, ubarₙ, zC)
 lines!(axvbar, vbarₙ, zC)
 lines!(axbbar, bbarₙ, zC)
@@ -433,42 +357,6 @@ lines!(axbbar, bbarₙ, zC)
 lines!(axuw, uwₙ, zF)
 lines!(axvw, vwₙ, zF)
 lines!(axwb, wbₙ, zF)
-
-lines!(axuadvection, u_udot∇uₙ, zC, label="Total")
-lines!(axuadvection, u_udot∇u′ₙ, zC, label="Sum of components")
-axislegend(axuadvection, position=:rb)
-
-lines!(axvadvection, v_udot∇vₙ, zC, label="Total")
-lines!(axvadvection, v_udot∇v′ₙ, zC, label="Sum of components")
-axislegend(axvadvection, position=:rb)
-
-lines!(axbadvection, b_udot∇bₙ, zC, label="Total")
-lines!(axbadvection, b_udot∇b′ₙ, zC, label="Sum of components")
-axislegend(axbadvection, position=:rb)
-
-lines!(axwadvection, w_udot∇wₙ, zC, label="Total")
-lines!(axwadvection, w_udot∇w′ₙ, zC, label="Sum of components")
-axislegend(axwadvection, position=:rb)
-
-lines!(axudissipation, u_∇dotνₑ∇uₙ, zC, label="Total")
-lines!(axudissipation, u_∇dotνₑ∇u′ₙ, zC, label="Sum of components")
-axislegend(axudissipation, position=:rb)
-
-lines!(axvdissipation, v_∇dotνₑ∇vₙ, zC, label="Total")
-lines!(axvdissipation, v_∇dotνₑ∇v′ₙ, zC, label="Sum of components")
-axislegend(axvdissipation, position=:rb)
-
-lines!(axbdissipation, b_∇dotκₑ∇bₙ, zC, label="Total")
-lines!(axbdissipation, b_∇dotκₑ∇b′ₙ, zC, label="Sum of components")
-axislegend(axbdissipation, position=:rb)
-
-lines!(axwdissipation, w_∇dotνₑ∇wₙ, zC, label="Total")
-lines!(axwdissipation, w_∇dotνₑ∇w′ₙ, zC, label="Sum of components")
-axislegend(axwdissipation, position=:rb)
-
-lines!(ax∂TKE∂t, ∂TKE∂tₙ, zC)
-
-lines!(ax∂b²∂t, ∂b²∂tₙ, zC)
 
 xlims!(axubar, ubarlim)
 xlims!(axvbar, vbarlim)
@@ -478,21 +366,7 @@ xlims!(axuw, uwlim)
 xlims!(axvw, vwlim)
 xlims!(axwb, wblim)
 
-xlims!(axuadvection, u_udot∇ulim)
-xlims!(axvadvection, v_udot∇vlim)
-xlims!(axbadvection, b_udot∇blim)
-xlims!(axwadvection, w_udot∇wlim)
-
-u_∇dotνₑ∇ulim != (0, 0) && xlims!(axudissipation, u_∇dotνₑ∇ulim)
-v_∇dotνₑ∇vlim != (0, 0) && xlims!(axvdissipation, v_∇dotνₑ∇vlim)
-b_∇dotκₑ∇blim != (0, 0) && xlims!(axbdissipation, b_∇dotκₑ∇blim)
-w_∇dotνₑ∇wlim != (0, 0) && xlims!(axwdissipation, w_∇dotνₑ∇wlim)
-
-xlims!(ax∂TKE∂t, ∂TKE∂tlim)
-xlims!(ax∂b²∂t, ∂b²∂tlim)
-
 trim!(fig.layout)
-display(fig)
 
 record(fig, "$(FILE_DIR)/$(FILE_NAME).mp4", 1:Nt, framerate=15) do nn
     n[] = nn
@@ -500,188 +374,3 @@ end
 
 @info "Animation completed"
 #%%
-#=
-b_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_fields.jld2", "b", backend=OnDisk())
-w_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_fields.jld2", "w", backend=OnDisk())
-
-ubar_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "ubar")
-vbar_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "vbar")
-bbar_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "bbar")
-
-uw_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "uw")
-vw_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "vw")
-wb_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "wb")
-
-∂u²∂t_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "∂u²∂t")
-∂v²∂t_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "∂v²∂t")
-∂w²∂t_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "∂w²∂t")
-∂b²∂t_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "∂b²∂t")
-
-∂u²∂t′_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "∂u²∂t′")
-∂v²∂t′_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "∂v²∂t′")
-∂w²∂t′_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "∂w²∂t′")
-∂b²∂t′_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "∂b²∂t′")
-
-b_∇dotκₑ∇b_2_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "b_∇dotκₑ∇b_2")
-
-Nt = length(bbar_data.times)
-
-xC = bbar_data.grid.xᶜᵃᵃ[1:Nx]
-yC = bbar_data.grid.yᵃᶜᵃ[1:Ny]
-zC = bbar_data.grid.zᵃᵃᶜ[1:Nz]
-
-zF = uw_data.grid.zᵃᵃᶠ[1:Nz+1]
-##
-fig = Figure(resolution=(1800, 1800))
-
-axb = Axis3(fig[1:2, 1:2], title="b", xlabel="x", ylabel="y", zlabel="z", viewmode=:fitzoom, aspect=:data)
-axw = Axis3(fig[1:2, 3:4], title="w", xlabel="x", ylabel="y", zlabel="z", viewmode=:fitzoom, aspect=:data)
-
-axubar = Axis(fig[3, 1], title="<u>", xlabel="m s⁻¹", ylabel="z")
-axvbar = Axis(fig[3, 2], title="<v>", xlabel="m s⁻¹", ylabel="z")
-axbbar = Axis(fig[3, 3], title="<b>", xlabel="m s⁻²", ylabel="z")
-
-axuw = Axis(fig[4, 1], title="uw", xlabel="m² s⁻²", ylabel="z")
-axvw = Axis(fig[4, 2], title="vw", xlabel="m² s⁻²", ylabel="z")
-axwb = Axis(fig[4, 3], title="wb", xlabel="m² s⁻³", ylabel="z")
-
-ax∂TKE∂tbar = Axis(fig[3, 4], title="0.5 <∂(u² + v² + w²)/∂t>", xlabel="m² s⁻³", ylabel="z")
-ax∂b²∂tbar = Axis(fig[4, 4], title="<∂b²/∂t>", xlabel="m² s⁻⁵", ylabel="z")
-
-xCs_xy = xC
-yCs_xy = yC
-zCs_xy = [zC[Nz] for x in xCs_xy, y in yCs_xy]
-
-yCs_yz = yC
-xCs_yz = range(xC[1], stop=xC[1], length=length(zC))
-zCs_yz = zeros(length(xCs_yz), length(yCs_yz))
-for j in axes(zCs_yz, 2)
-  zCs_yz[:, j] .= zC
-end
-
-xCs_xz = xC
-yCs_xz = range(yC[1], stop=yC[1], length=length(zC))
-zCs_xz = zeros(length(xCs_xz), length(yCs_xz))
-for i in axes(zCs_xz, 1)
-  zCs_xz[i, :] .= zC
-end
-
-xFs_xy = xC
-yFs_xy = yC
-zFs_xy = [zF[Nz+1] for x in xFs_xy, y in yFs_xy]
-
-yFs_yz = yC
-xFs_yz = range(xC[1], stop=xC[1], length=length(zF))
-zFs_yz = zeros(length(xFs_yz), length(yFs_yz))
-for j in axes(zFs_yz, 2)
-  zFs_yz[:, j] .= zF
-end
-
-xFs_xz = xC
-yFs_xz = range(yC[1], stop=yC[1], length=length(zF))
-zFs_xz = zeros(length(xFs_xz), length(yFs_xz))
-for i in axes(zFs_xz, 1)
-  zFs_xz[i, :] .= zF
-end
-
-function find_min(a...)
-    return minimum(minimum.([a...]))
-end
-
-function find_max(a...)
-    return maximum(maximum.([a...]))
-end
-
-blim = (minimum(b_data), maximum(b_data))
-wlim = (minimum(w_data), maximum(w_data))
-
-colormap = Reverse(:RdBu_10)
-b_color_range = blim
-w_color_range = wlim
-
-ubarlim = (minimum(ubar_data), maximum(ubar_data))
-vbarlim = (minimum(vbar_data), maximum(vbar_data))
-bbarlim = (minimum(bbar_data), maximum(bbar_data))
-
-∂b²∂tlim = (find_min(∂b²∂t_data, ∂b²∂t′_data), 
-            find_max(∂b²∂t_data, ∂b²∂t′_data))
-∂TKE∂tlim = (find_min(0.5 .* (∂u²∂t_data .+ ∂v²∂t_data .+ ∂w²∂t_data), 0.5 .* (∂u²∂t′_data .+ ∂v²∂t′_data .+ ∂w²∂t′_data)), 
-             find_max(0.5 .* (∂u²∂t_data .+ ∂v²∂t_data .+ ∂w²∂t_data), 0.5 .* (∂u²∂t′_data .+ ∂v²∂t′_data .+ ∂w²∂t′_data)))
-
-startframe_lim = 30
-uwlim = (minimum(uw_data[1, 1, :, startframe_lim:end]), maximum(uw_data[1, 1, :, startframe_lim:end]))
-vwlim = (minimum(vw_data[1, 1, :, startframe_lim:end]), maximum(vw_data[1, 1, :, startframe_lim:end]))
-wblim = (minimum(wb_data[1, 1, :, startframe_lim:end]), maximum(wb_data[1, 1, :, startframe_lim:end]))
-
-n = Observable(1)
-
-bₙ_xy = @lift interior(b_data[$n], :, :, Nz)
-bₙ_yz = @lift transpose(interior(b_data[$n], 1, :, :))
-bₙ_xz = @lift interior(b_data[$n], :, 1, :)
-
-wₙ_xy = @lift interior(w_data[$n], :, :, Nz+1)
-wₙ_yz = @lift transpose(interior(w_data[$n], 1, :, :))
-wₙ_xz = @lift interior(w_data[$n], :, 1, :)
-
-time_str = @lift "Qᵁ = $(Qᵁ), Qᴮ = $(Qᴮ), Time = $(round(bbar_data.times[$n]/24/60^2, digits=3)) days"
-title = Label(fig[0, :], time_str, font=:bold, tellwidth=false)
-
-b_xy_surface = surface!(axb, xCs_xy, yCs_xy, zCs_xy, color=bₙ_xy, colormap=colormap, colorrange = b_color_range)
-b_yz_surface = surface!(axb, xCs_yz, yCs_yz, zCs_yz, color=bₙ_yz, colormap=colormap, colorrange = b_color_range)
-b_xz_surface = surface!(axb, xCs_xz, yCs_xz, zCs_xz, color=bₙ_xz, colormap=colormap, colorrange = b_color_range)
-
-w_xy_surface = surface!(axw, xFs_xy, yFs_xy, zFs_xy, color=wₙ_xy, colormap=colormap, colorrange = w_color_range)
-w_yz_surface = surface!(axw, xFs_yz, yFs_yz, zFs_yz, color=wₙ_yz, colormap=colormap, colorrange = w_color_range)
-w_xz_surface = surface!(axw, xFs_xz, yFs_xz, zFs_xz, color=wₙ_xz, colormap=colormap, colorrange = w_color_range)
-
-ubarₙ = @lift interior(ubar_data[$n], 1, 1, :)
-vbarₙ = @lift interior(vbar_data[$n], 1, 1, :)
-bbarₙ = @lift interior(bbar_data[$n], 1, 1, :)
-
-uwₙ = @lift interior(uw_data[$n], 1, 1, :)
-vwₙ = @lift interior(vw_data[$n], 1, 1, :)
-wbₙ = @lift interior(wb_data[$n], 1, 1, :)
-
-∂TKE∂tbarₙ = @lift 0.5 .* (interior(∂u²∂t_data[$n], 1, 1, :) .+ interior(∂v²∂t_data[$n], 1, 1, :) .+ interior(∂w²∂t_data[$n], 1, 1, :))
-∂TKE∂t′barₙ = @lift 0.5 .* (interior(∂u²∂t′_data[$n], 1, 1, :) .+ interior(∂v²∂t′_data[$n], 1, 1, :) .+ interior(∂w²∂t′_data[$n], 1, 1, :))
-
-∂b²∂tbarₙ = @lift interior(∂b²∂t_data[$n], 1, 1, :)
-∂b²∂t′barₙ = @lift interior(∂b²∂t′_data[$n], 1, 1, :)
-
-lines!(axubar, ubarₙ, zC)
-lines!(axvbar, vbarₙ, zC)
-lines!(axbbar, bbarₙ, zC)
-
-lines!(axuw, uwₙ, zF)
-lines!(axvw, vwₙ, zF)
-lines!(axwb, wbₙ, zF)
-
-lines!(ax∂TKE∂tbar, ∂TKE∂tbarₙ, zC, label="Total")
-lines!(ax∂TKE∂tbar, ∂TKE∂t′barₙ, zC, label="Sum of components")
-axislegend(ax∂TKE∂tbar, position=:rb)
-
-lines!(ax∂b²∂tbar, ∂b²∂tbarₙ, zC, label="Total")
-lines!(ax∂b²∂tbar, ∂b²∂t′barₙ, zC, label="Sum of components")
-axislegend(ax∂b²∂tbar, position=:rb)
-
-
-xlims!(axubar, ubarlim)
-xlims!(axvbar, vbarlim)
-xlims!(axbbar, bbarlim)
-
-xlims!(axuw, uwlim)
-xlims!(axvw, vwlim)
-xlims!(axwb, wblim)
-
-xlims!(ax∂TKE∂tbar, ∂TKE∂tlim)
-xlims!(ax∂b²∂tbar, ∂b²∂tlim)
-
-trim!(fig.layout)
-
-record(fig, "$(FILE_DIR)/video.mp4", 1:Nt, framerate=15) do nn
-    n[] = nn
-end
-
-@info "Animation completed"
-#%%
-=#

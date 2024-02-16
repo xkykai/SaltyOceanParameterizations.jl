9e90446370f52562b4c59a3a678f4350e07c983a
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-02-16 16:14:29 -0500

    Add DataWrangling module and feature scaling functions

---
+++ src/DataWrangling/DataWrangling.jl
@@ -0,0 +1,7 @@
+module DataWrangling
+export ZeroMeanUnitVarianceScaling, MinMaxScaling
+
+include("feature_scaling.jl")
+include("LES_data_processing.jl")
+
+end
--- src/SaltyOceanParameterizations.jl
+++ src/SaltyOceanParameterizations.jl
@@ -1,5 +1,6 @@
 module SaltyOceanParameterizations
 
 # Write your package code here.
+include("DataWrangling/DataWrangling.jl")
 
 end

f62d36d07717c7d83221a679b1c00228e3a45077
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-03-15 17:07:36 -0400

    Update FILE_DIR and neural network architecture

--- train_slightly_nonlocal_diffusivity_NDE_gradient.jl
+++ train_slightly_nonlocal_diffusivity_NDE_gradient.jl
@@ -19,7 +19,7 @@ function find_max(a...)
     return maximum(maximum.([a...]))
 end
 
-FILE_DIR = "./training_output/slightly_nonlocal_diffusivity_NDE_gradient_tanh_noclamp"
+FILE_DIR = "./training_output/slightly_nonlocal_diffusivity_NDE_gradient_tanh_noclamp_small"
 @info FILE_DIR
 mkpath(FILE_DIR)
 
@@ -41,7 +41,7 @@ train_data_plot = LESDatasets(field_datasets, ZeroMeanUnitVarianceScaling, full_
 
 rng = Random.default_rng(123)
 
-NN = Chain(Dense(2, 16, tanh_fast), Dense(16, 2, tanh_fast), x -> x.+ 1)
+NN = Chain(Dense(2, 4, tanh_fast), Dense(4, 2, relu))
 
 ps_NN, st_NN = Lux.setup(rng, NN)
 ps_NN = ps_NN |> ComponentArray .|> Float64

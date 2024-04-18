47b7fa3bf1afbce9b6a5e91de67d42da951a1531
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-04-17 21:45:48 -0400

    Refactor calculate_Ri functions to include an optional parameter for ϵ

--- src/base_closure.jl
+++ src/base_closure.jl
@@ -1,8 +1,7 @@
 using SeawaterPolynomials.TEOS10
 using NNlib: tanh_fast
 
-function calculate_Ri(u, v, T, S, Dᶠ, g, ρ₀; clamp_lims=(-Inf, Inf))
-    ϵ = 1e-7
+function calculate_Ri(u, v, T, S, Dᶠ, g, ρ₀; clamp_lims=(-Inf, Inf), ϵ=1e-7)
     eos = TEOS10EquationOfState()
     ρ′ = TEOS10.ρ′.(T, S, 0, Ref(eos))
     ∂ρ∂z = Dᶠ * ρ′
@@ -13,8 +12,7 @@ function calculate_Ri(u, v, T, S, Dᶠ, g, ρ₀; clamp_lims=(-Inf, Inf))
     return clamp.(∂b∂z ./ (∂u∂z.^2 .+ ∂v∂z.^2 .+ ϵ), clamp_lims[1], clamp_lims[2])
 end
 
-function calculate_Ri(u, v, ρ, Dᶠ, g, ρ₀; clamp_lims=(-Inf, Inf))
-    ϵ = 1e-7
+function calculate_Ri(u, v, ρ, Dᶠ, g, ρ₀; clamp_lims=(-Inf, Inf), ϵ=1e-7)
     ∂ρ∂z = Dᶠ * ρ
     ∂u∂z = Dᶠ * u
     ∂v∂z = Dᶠ * v
@@ -23,9 +21,7 @@ function calculate_Ri(u, v, ρ, Dᶠ, g, ρ₀; clamp_lims=(-Inf, Inf))
     return clamp.(∂b∂z ./ (∂u∂z.^2 .+ ∂v∂z.^2 .+ ϵ), clamp_lims[1], clamp_lims[2])
 end
 
-function calculate_Ri!(Ri, u, v, ρ, ∂b∂z, ∂u∂z, ∂v∂z, g, ρ₀, Δ; clamp_lims=(-Inf, Inf))
-    ϵ = 1e-7
-    
+function calculate_Ri!(Ri, u, v, ρ, ∂b∂z, ∂u∂z, ∂v∂z, g, ρ₀, Δ; clamp_lims=(-Inf, Inf), ϵ=1e-7)
     Dᶠ!(∂u∂z, u, Δ)
     Dᶠ!(∂v∂z, v, Δ)
     
@@ -60,7 +56,6 @@ end
 function local_Ri_κ_piecewise_linear(ν, Pr)
     return ν / Pr
 end
-
 
 function nonlocal_Ri_diffusivity(ν, D⁺, ν₁_en, Δν_enᶜ, ΔΔν_en, Pr)
     ν⁺ = D⁺ * ν
@@ -116,5 +111,23 @@ function local_Ri_κ_convectivetanh_shearlinear(Ri, ν_conv, ν_shear, m, ΔRi, 
 end
 
 function local_Ri_κ_convectivetanh_shearlinear(ν, Pr)
+    return ν / Pr
+end
+
+function nonlocal_Ri_ν_convectivetanh_shearlinear(Ri, Ri_above, ∂ρ∂z, Qρ, ν_conv, ν_shear, m, ΔRi, C_en, x₀, Δx, ϵ=1e-11)
+    ν_local = local_Ri_ν_convectivetanh_shearlinear(Ri, ν_conv, ν_shear, m, ΔRi)
+    entrainment = Ri > 0 && Ri_above < 0 && Qρ < 0
+    x = Qρ / (∂ρ∂z + ϵ)
+    ν_nonlocal = entrainment * C_en * ν_conv * 0.5 * (tanh((x - x₀) / Δx) + 1)
+
+    return ν_local + ν_nonlocal
+end
+
+function nonlocal_Ri_κ_convectivetanh_shearlinear(Ri, Ri_above, ∂ρ∂z, Qρ, ν_conv, ν_shear, m, ΔRi, C_en, x₀, Δx, Pr, ϵ=1e-11)
+    ν = nonlocal_Ri_ν_convectivetanh_shearlinear(Ri, Ri_above, ∂ρ∂z, Qρ, ν_conv, ν_shear, m, ΔRi, C_en, x₀, Δx, ϵ)
+    return ν / Pr
+end
+
+function nonlocal_Ri_κ_convectivetanh_shearlinear(ν, Pr)
     return ν / Pr
 end

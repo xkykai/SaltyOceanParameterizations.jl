b83f437f2f180c2d51a672c34a55e3602f0af005
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-04-17 22:38:24 -0400

    Refactor calculate_Ri functions to include an optional parameter for ϵ

--- src/base_closure.jl
+++ src/base_closure.jl
@@ -1,7 +1,8 @@
 using SeawaterPolynomials.TEOS10
 using NNlib: tanh_fast
 
-function calculate_Ri(u, v, T, S, Dᶠ, g, ρ₀; clamp_lims=(-Inf, Inf), ϵ=1e-7)
+function calculate_Ri(u, v, T, S, Dᶠ, g, ρ₀; clamp_lims=(-Inf, Inf), ϵ=1e-11)
+    # ϵ = 1e-7
     eos = TEOS10EquationOfState()
     ρ′ = TEOS10.ρ′.(T, S, 0, Ref(eos))
     ∂ρ∂z = Dᶠ * ρ′
@@ -12,7 +13,8 @@ function calculate_Ri(u, v, T, S, Dᶠ, g, ρ₀; clamp_lims=(-Inf, Inf), ϵ=1e-
     return clamp.(∂b∂z ./ (∂u∂z.^2 .+ ∂v∂z.^2 .+ ϵ), clamp_lims[1], clamp_lims[2])
 end
 
-function calculate_Ri(u, v, ρ, Dᶠ, g, ρ₀; clamp_lims=(-Inf, Inf), ϵ=1e-7)
+function calculate_Ri(u, v, ρ, Dᶠ, g, ρ₀; clamp_lims=(-Inf, Inf), ϵ=1e-11)
+    # ϵ = 1e-7
     ∂ρ∂z = Dᶠ * ρ
     ∂u∂z = Dᶠ * u
     ∂v∂z = Dᶠ * v
@@ -21,7 +23,8 @@ function calculate_Ri(u, v, ρ, Dᶠ, g, ρ₀; clamp_lims=(-Inf, Inf), ϵ=1e-7)
     return clamp.(∂b∂z ./ (∂u∂z.^2 .+ ∂v∂z.^2 .+ ϵ), clamp_lims[1], clamp_lims[2])
 end
 
-function calculate_Ri!(Ri, u, v, ρ, ∂b∂z, ∂u∂z, ∂v∂z, g, ρ₀, Δ; clamp_lims=(-Inf, Inf), ϵ=1e-7)
+function calculate_Ri!(Ri, u, v, ρ, ∂b∂z, ∂u∂z, ∂v∂z, g, ρ₀, Δ; clamp_lims=(-Inf, Inf), ϵ=1e-11)
+    # ϵ = 1e-7
     Dᶠ!(∂u∂z, u, Δ)
     Dᶠ!(∂v∂z, v, Δ)
     

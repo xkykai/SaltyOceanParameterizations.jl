e9640e0d3bc3cb9a928fef1461cd51b02be26751
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-05-30 00:12:48 -0400

    Update FILE_DIR in train_NDE_enzyme_slightlylocal_NN.jl

--- train_NDE_enzyme_slightlylocal_NN.jl
+++ train_NDE_enzyme_slightlylocal_NN.jl
@@ -62,7 +62,7 @@ const S_scaling = args["S_scaling"]
 
 LES_FILE_DIRS = ["./LES2/$(file)/instantaneous_timeseries.jld2" for file in LES_suite["train22new"]]
 
-FILE_DIR = "./training_output/NDE_$(length(LES_FILE_DIRS))sim_$(args["hidden_layer"])layer_$(args["hidden_layer_size"])_$(args["activation"])"
+FILE_DIR = "./training_output/NDE_$(length(LES_FILE_DIRS))simnew_$(args["hidden_layer"])layer_$(args["hidden_layer_size"])_$(args["activation"])"
 mkpath(FILE_DIR)
 @info FILE_DIR
 

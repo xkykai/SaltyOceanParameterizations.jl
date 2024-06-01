bd0cd36f41a2d2d913703a1e46e95ac05eed8a01
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-06-01 00:50:56 -0400

    Update LES_FILE_DIRS to use train34new instead of train22new

--- train_localbaseclosure_convectivetanh_shearlinear_TSrho_EKI.jl
+++ train_localbaseclosure_convectivetanh_shearlinear_TSrho_EKI.jl
@@ -39,7 +39,7 @@ args = parse_commandline()
 LES_FILE_DIRS = ["./LES2/$(file)/instantaneous_timeseries.jld2" for file in LES_suite["train34new"]]
 const S_scaling = args["S_scaling"]
 const momentum_ratio = args["momentum_ratio"]
-FILE_DIR = "./training_output/$(length(LES_FILE_DIRS))simnew_mom_$(momentum_ratio)_localbaseclosure_convectivetanh_shearlinear_EKI_oldprior"
+FILE_DIR = "./training_output/$(length(LES_FILE_DIRS))simnew_mom_$(momentum_ratio)_localbaseclosure_convectivetanh_shearlinear_EKI_oldprior_stop"
 mkpath(FILE_DIR)
 
 field_datasets = [FieldDataset(FILE_DIR, backend=OnDisk()) for FILE_DIR in LES_FILE_DIRS]
@@ -613,7 +613,7 @@ prior_ΔRi = constrained_gaussian("ΔRi", ps_prior.ΔRi, ps_prior.ΔRi/5, -Inf, 
 priors = combine_distributions([prior_ν_conv, prior_ν_shear, prior_m, prior_Pr, prior_ΔRi])
 target = [0.]
 
-N_ensemble = 100
+N_ensemble = 200
 N_iterations = 2000
 Γ = prior_loss / 1e6 * I
 
@@ -621,7 +621,8 @@ ps_eki = EKP.construct_initial_ensemble(rng, priors, N_ensemble)
 ensemble_kalman_process = EKP.EnsembleKalmanProcess(ps_eki, target, Γ, Inversion(); 
                                                     rng = rng, 
                                                     failure_handler_method = SampleSuccGauss(), 
-                                                    scheduler = DataMisfitController(on_terminate="continue"))
+                                                    scheduler = DataMisfitController(on_terminate="stop"))
+                                                    # scheduler = DataMisfitController(on_terminate="continue"))
 G_ens = zeros(1, N_ensemble)
 
 losses = []

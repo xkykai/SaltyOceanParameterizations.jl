b4d82d0a7b137cf61bceff1738a2159952137c5f
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-06-04 16:18:40 -0400

    Update LES_FILE_DIRS to use train34new instead of train22new

--- train_NDE_enzyme_slightlylocal_NN_localbaseclosure_convectivetanh_shearlinear.jl
+++ train_NDE_enzyme_slightlylocal_NN_localbaseclosure_convectivetanh_shearlinear.jl
@@ -65,7 +65,7 @@ end
 const S_scaling = args["S_scaling"]
 const momentum_ratio = args["momentum_ratio"]
 
-LES_FILE_DIRS = ["./LES2/$(file)/instantaneous_timeseries.jld2" for file in LES_suite["train22new"]]
+LES_FILE_DIRS = ["./LES2/$(file)/instantaneous_timeseries.jld2" for file in LES_suite["train34new"]]
 
 FILE_DIR = "./training_output/NDE_$(length(LES_FILE_DIRS))simnew_$(args["hidden_layer"])layer_$(args["hidden_layer_size"])_$(args["activation"])_mom_$(momentum_ratio)_localbaseclosure_2"
 mkpath(FILE_DIR)

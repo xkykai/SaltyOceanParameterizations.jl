fcf7969344b614e2f5eae1eded715bda94782fbe
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-07-18 17:09:43 -0400

    Update LES_suite.jl with new training configurations

--- src/TrainingOperations/LES_suite.jl
+++ src/TrainingOperations/LES_suite.jl
@@ -179,6 +179,92 @@ LES_suite = Dict(
         "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0002_QT_0.0003_QS_-3.0e-5_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
     ],
 
+    "train42new" => [
+        # Midlatitude Atlantic
+        # Pure Wind Stress
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_-0.0005_QT_0.0_QS_0.0_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_-0.0002_QT_0.0_QS_0.0_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+    
+        # Free Convection
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_0.0_QT_0.0005_QS_0.0_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_0.0_QT_0.0001_QS_0.0_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+    
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_0.0_QT_0.0_QS_-5.0e-5_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_0.0_QT_0.0_QS_-1.0e-5_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_0.0_QT_0.0003_QS_-3.0e-5_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # No Convection
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_0.0_QT_0.0_QS_0.0_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # negative dSdz
+        "linearTS_dTdz_0.014_dSdz_-0.0021_QU_0.0_QT_0.0003_QS_-3.0e-5_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # high rotation
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_0.0_QT_0.0003_QS_-3.0e-5_T_18.0_S_36.6_f_0.00015_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # T_surface = 10°C
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_0.0_QT_0.0003_QS_-3.0e-5_T_10.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # Wind Stress and Free Convection
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_-0.0002_QT_0.0005_QS_0.0_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_-0.0002_QT_0.0001_QS_0.0_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_-0.0005_QT_0.0005_QS_0.0_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_-0.0005_QT_0.0001_QS_0.0_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_-0.0002_QT_0.0_QS_-5.0e-5_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_-0.0002_QT_0.0_QS_-1.0e-5_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_-0.0005_QT_0.0_QS_-5.0e-5_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_-0.0005_QT_0.0_QS_-1.0e-5_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_-0.0005_QT_0.0003_QS_-3.0e-5_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.014_dSdz_0.0021_QU_-0.0002_QT_0.0003_QS_-3.0e-5_T_18.0_S_36.6_f_8.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # Equatorial Pacific
+        # Pure Wind Stress
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0005_QT_0.0_QS_0.0_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0002_QT_0.0_QS_0.0_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # Free Convection
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_0.0_QT_0.0005_QS_0.0_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_0.0_QT_0.0001_QS_0.0_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+    
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_0.0_QT_0.0_QS_-5.0e-5_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_0.0_QT_0.0_QS_-1.0e-5_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_0.0_QT_0.0003_QS_-3.0e-5_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # No Convection
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_0.0_QT_0.0_QS_0.0_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # negative dSdz
+        "linearTS_dTdz_0.013_dSdz_-0.00075_QU_0.0_QT_0.0003_QS_-3.0e-5_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # high rotation
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_0.0_QT_0.0003_QS_-3.0e-5_T_14.5_S_35.0_f_0.00015_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # T_surface = 10°C
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_0.0_QT_5e-5_QS_-5.0e-6_T_10.0_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # Wind Stress and Free Convection
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0002_QT_0.0005_QS_0.0_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0002_QT_0.0001_QS_0.0_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0005_QT_0.0005_QS_0.0_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0005_QT_0.0001_QS_0.0_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0002_QT_0.0_QS_-5.0e-5_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0002_QT_0.0_QS_-1.0e-5_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0005_QT_0.0_QS_-5.0e-5_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0005_QT_0.0_QS_-1.0e-5_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0005_QT_0.0003_QS_-3.0e-5_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_-0.0002_QT_0.0003_QS_-3.0e-5_T_14.5_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+    ],
+
     "train51new" => [
         # Midlatitude Atlantic
         # Pure Wind Stress
@@ -528,7 +614,7 @@ LES_suite = Dict(
         "linearTS_dTdz_0.013_dSdz_0.00075_QU_0.0_QT_0.0003_QS_-3.0e-5_T_14.5_S_35.0_f_0.00015_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
 
         # T_surface = 10°C
-        "linearTS_dTdz_0.013_dSdz_0.00075_QU_0.0_QT_0.0003_QS_-3.0e-5_T_10.0_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.013_dSdz_0.00075_QU_0.0_QT_5e-5_QS_-5.0e-6_T_10.0_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
 
         # Southern Ocean
         # Free Convection

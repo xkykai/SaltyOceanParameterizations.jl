b2608e51d3d22f440e094ebff81bb03b0a11e7a4
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-09-30 16:08:55 -0400

    train on 54 sim as backup

--- train_NDE_enzyme_slightlylocal_NN_Qb_nof_BBLintegral_localbaseclosure_convectivetanh_2Pr_shearlinear_wTwS_nouvloss_SGD.jl
+++ train_NDE_enzyme_slightlylocal_NN_Qb_nof_BBLintegral_localbaseclosure_convectivetanh_2Pr_shearlinear_wTwS_nouvloss_SGD.jl
@@ -76,7 +76,7 @@ const S_scaling = args["S_scaling"]
 seed = args["random_seed"]
 learning_rate = args["learning_rate"]
 
-LES_FILE_DIRS = ["./LES2/$(file)/instantaneous_timeseries.jld2" for file in LES_suite["train64new"]]
+LES_FILE_DIRS = ["./LES2/$(file)/instantaneous_timeseries.jld2" for file in LES_suite["train54new"]]
 
 FILE_DIR = "./training_output/NDE_Qb_dt20min_nof_BBLintegral_wTwS_$(length(LES_FILE_DIRS))simnew_$(args["hidden_layer"])layer_$(args["hidden_layer_size"])_$(args["activation"])_$(seed)seed_$(learning_rate)lr_localbaseclosure_2Pr_6simstableRi_SGD"
 mkpath(FILE_DIR)

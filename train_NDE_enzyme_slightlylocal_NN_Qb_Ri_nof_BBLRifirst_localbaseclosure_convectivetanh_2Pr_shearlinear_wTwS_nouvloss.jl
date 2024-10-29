4abb0eadb8497e0c6c91eb8dee236423eff12d55
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-10-29 14:42:56 -0400

    fix training suite

--- train_NDE_enzyme_slightlylocal_NN_Qb_Ri_nof_BBLRifirst_localbaseclosure_convectivetanh_2Pr_shearlinear_wTwS_nouvloss.jl
+++ train_NDE_enzyme_slightlylocal_NN_Qb_Ri_nof_BBLRifirst_localbaseclosure_convectivetanh_2Pr_shearlinear_wTwS_nouvloss.jl
@@ -76,7 +76,7 @@ const grid_point_below_kappa = args["point_below_kappa"]
 seed = args["random_seed"]
 learning_rate = args["learning_rate"]
 
-LES_FILE_DIRS = ["./LES2/$(file)/instantaneous_timeseries.jld2" for file in LES_suite["train34new"]]
+LES_FILE_DIRS = ["./LES2/$(file)/instantaneous_timeseries.jld2" for file in LES_suite["trainnoSO34new"]]
 
 FILE_DIR = "./training_output/NDE_Qb_Ri_nof_BBLRifirst$(grid_point_below_kappa)_wTwS_$(length(LES_FILE_DIRS))simnew_$(args["hidden_layer"])layer_$(args["hidden_layer_size"])_$(args["activation"])_$(seed)seed_$(learning_rate)lr_localbaseclosure_2Pr_6simstableRi"
 mkpath(FILE_DIR)

535d2a45752fb8dfd65e2f921e9b949a54f0c205
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-11-13 00:59:57 -0500

    extend compute_density_contribution

--- src/TrainingOperations/loss_operations.jl
+++ src/TrainingOperations/loss_operations.jl
@@ -25,3 +25,23 @@ function compute_density_contribution(data::LESData)
 
     return (; T=T_contribution, S=S_contribution, ρ=Δρ)
 end
+
+function compute_density_contribution(data::LESData, index)
+    eos = TEOS10EquationOfState()
+    ρ = data.profile.ρ.unscaled[:, index]
+    T = data.profile.T.unscaled[:, index]
+    S = data.profile.S.unscaled[:, index]
+
+    Δρ = maximum(ρ) - minimum(ρ)
+    ΔT = maximum(T) - minimum(T)
+    ΔS = maximum(S) - minimum(S)
+
+    α = mean(SeawaterPolynomials.thermal_expansion.(T, S, 0, Ref(eos)))
+    β = mean(SeawaterPolynomials.haline_contraction.(T, S, 0, Ref(eos)))
+    ρ₀ = eos.reference_density
+
+    T_contribution = α * ΔT * ρ₀
+    S_contribution = β * ΔS * ρ₀
+
+    return (; T=T_contribution, S=S_contribution, ρ=Δρ)
+end

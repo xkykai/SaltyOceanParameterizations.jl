82ff60de360da2a8ad35861328e1aa34925d6b5a
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-12-20 00:37:11 -0500

    update LES suite

--- src/TrainingOperations/LES_suite.jl
+++ src/TrainingOperations/LES_suite.jl
@@ -2767,6 +2767,59 @@ LES_suite = Dict(
         "linearTS_dTdz_0.05_dSdz_0.007_QU_-0.0004_QT_0.0004_QS_-4.0e-5_T_25.0_S_36.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
         "linearTS_dTdz_0.03_dSdz_0.0075_QU_-0.0002_QT_0.0003_QS_-2.0e-5_T_27.0_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
     ],
+
+    "validate30new2" => [
+        # Midlatitude Atlantic
+        # Free Convection
+        "linearTS_dTdz_0.015_dSdz_0.002_QU_0.0_QT_0.00035_QS_0.0_T_20.0_S_37.0_f_0.0001_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.015_dSdz_0.002_QU_0.0_QT_0.0_QS_-3.5e-5_T_20.0_S_37.0_f_0.0001_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # Winds + Convection
+        "linearTS_dTdz_0.015_dSdz_0.002_QU_-0.0001_QT_0.0002_QS_-2.0e-5_T_20.0_S_37.0_f_0.0001_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.01_dSdz_0.0015_QU_-0.00035_QT_0.0002_QS_-1.5e-5_T_17.0_S_36.0_f_5.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.017_dSdz_0.0018_QU_-0.00025_QT_0.0004_QS_-4.0e-5_T_12.0_S_37.0_f_2.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.012_dSdz_0.0012_QU_-0.0004_QT_5.0e-5_QS_-5.0e-6_T_10.0_S_37.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.01_dSdz_-0.002_QU_-0.0003_QT_0.00035_QS_-3.5e-5_T_16.0_S_34.0_f_0.00015_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.013_dSdz_-0.001_QU_-0.00045_QT_0.00045_QS_-4.0e-5_T_13.0_S_35.0_f_7.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # Equatorial Pacific
+        # Free Convection
+        "linearTS_dTdz_0.01_dSdz_0.0005_QU_0.0_QT_0.00035_QS_0.0_T_17.0_S_34.5_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.01_dSdz_0.0005_QU_0.0_QT_0.0_QS_-3.5e-5_T_17.0_S_34.5_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # Winds + Convection
+        "linearTS_dTdz_0.01_dSdz_0.0005_QU_-0.0001_QT_0.0002_QS_-2.0e-5_T_17.0_S_34.5_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.012_dSdz_0.0007_QU_-0.00035_QT_0.0002_QS_-1.5e-5_T_13.0_S_36.0_f_-1.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.016_dSdz_0.0006_QU_-0.00025_QT_0.0004_QS_-4.0e-5_T_16.0_S_34.0_f_-2.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.011_dSdz_0.0003_QU_-0.0004_QT_5.0e-5_QS_-5.0e-6_T_10.0_S_36.5_f_3.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.015_dSdz_-0.0005_QU_-0.0003_QT_0.00035_QS_-3.5e-5_T_20.0_S_37.0_f_-5.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.017_dSdz_-0.0008_QU_-0.00045_QT_0.00045_QS_-4.0e-5_T_12.0_S_35.0_f_-0.0001_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # Southern Ocean
+        # Free Convection
+        "linearTS_dTdz_-0.02_dSdz_-0.0047_QU_0.0_QT_0.00035_QS_0.0_T_0.0_S_34.5_f_-0.00015_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_-0.02_dSdz_-0.0047_QU_0.0_QT_0.0_QS_-3.5e-5_T_0.0_S_34.5_f_-0.00015_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # Winds + Convection
+        "linearTS_dTdz_-0.02_dSdz_-0.0047_QU_-0.0001_QT_0.0002_QS_-2.0e-5_T_0.0_S_34.5_f_-0.00015_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_-0.017_dSdz_-0.004_QU_-0.00035_QT_0.0002_QS_-1.5e-5_T_3.0_S_34.0_f_0.00015_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_-0.015_dSdz_-0.004_QU_-0.00025_QT_0.0004_QS_-4.0e-5_T_-1.0_S_36.0_f_0.0001_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_-0.01_dSdz_-0.003_QU_-0.0004_QT_5.0e-5_QS_-5.0e-6_T_0.0_S_33.5_f_0.000125_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_-0.02_dSdz_-0.004_QU_-0.0003_QT_0.00035_QS_-3.5e-5_T_1.0_S_35.5_f_-0.00014_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_-0.02_dSdz_-0.0045_QU_-0.00045_QT_0.00045_QS_-4.0e-5_T_2.0_S_34.0_f_-0.0001_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # Misc cases where there is no salinity gradient
+        "linearTS_dTdz_0.03_dSdz_0.0_QU_-0.0002_QT_0.00045_QS_0.0_T_20.0_S_37.0_f_0.0001_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.01_dSdz_0.0_QU_-0.0003_QT_0.00015_QS_0.0_T_15.0_S_35.0_f_5.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        
+        # Misc cases where there is no temperature gradient
+        "linearTS_dTdz_0.0_dSdz_-0.0045_QU_-0.0003_QT_0.0_QS_-4.0e-5_T_17.0_S_36.0_f_-0.0001_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.0_dSdz_-0.0025_QU_-0.00015_QT_0.0_QS_-2.0e-5_T_16.0_S_34.0_f_-1.0e-5_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+
+        # Very warm, very stratified
+        "linearTS_dTdz_0.05_dSdz_0.007_QU_-0.0004_QT_0.0004_QS_-4.0e-5_T_25.0_S_36.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+        "linearTS_dTdz_0.03_dSdz_0.0075_QU_-0.0002_QT_0.0003_QS_-2.0e-5_T_27.0_S_35.0_f_0.0_WENO9nu0_Lxz_512.0_256.0_Nxz_256_128",
+    ],
     
     "validate37new" => [
         # Midlatitude Atlantic
